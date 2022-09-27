f6f4b4499c959a2e30057119b57c29894ceae1c1
Jimmy Jouanne <jimmy.jouanne@wttj.co>
2022-09-27 08:28:46 +0200

    chore: style guide configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 function App() {
-  return <div className="App">Hello world !</div>;
+  return <div className="bg-neutral-dark-blue min-h-screen">Hello world !</div>;
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          'light-cyan': 'hsl(193, 38%, 86%)',
+          'neon-cyan': 'hsl(150, 100%, 66%)',
+        },
+        neutral: {
+          'grayish-blue': 'hsl(217, 19%, 38%)',
+          'dark-grayish-blue': 'hsl(217, 19%, 24%)',
+          'dark-blue': 'hsl(218, 23%, 16%)',
+        },
+      },
+      screens: {
+        tablet: '640px',
+        laptop: '1024px',
+        desktop: '1440px',
+      },
+    },
   },
   plugins: [],
 };
