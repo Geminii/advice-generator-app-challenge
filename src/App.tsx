92bac300e11afc5fcd56d93784d465fea5ba95ce
Jimmy Jouanne <jimmy.jouanne@wttj.co>
2022-09-27 13:57:28 +0200

    feat: advice card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,47 @@
 import { AdviceCard } from 'components/AdviceCard';
+import { useEffect, useState } from 'react';
+
+const ADVICE_API_URL = 'https://api.adviceslip.com';
+
+export type AdviceType = {
+  title: string;
+  content: string;
+};
+
+type AdviceResponseType = {
+  slip: {
+    id: number;
+    advice: string;
+  };
+};
 
 function App() {
+  const [advice, setAdvice] = useState<AdviceType | null>(null);
+  const [error, setError] = useState(false);
+
+  const fetchAdvice = async () => {
+    await fetch(`${ADVICE_API_URL}/advice`)
+      .then((response) => response.json())
+      .then(({ slip }: AdviceResponseType) => {
+        setError(false);
+        setAdvice({
+          title: `Advice #${slip.id}`,
+          content: slip.advice,
+        });
+      })
+      .catch(() => setError(true));
+  };
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
   return (
     <div className="bg-neutral-dark-blue min-h-screen flex items-center justify-center px-4">
-      <AdviceCard
-        title="Advice #117"
-        advice="It is easy to sit up and take notice, what's difficult is getting up and taking action."
-      />
+      {error && <AdviceCard title="Advice error" content="Oops, something went wrong." />}
+      {advice && (
+        <AdviceCard title={advice.title} content={advice.content} handleClick={fetchAdvice} />
+      )}
     </div>
   );
 }
--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -1,23 +1,34 @@
-import { ReactComponent as PatternDividerIcon } from 'assets/pattern-divider-desktop.svg';
+import { ReactComponent as PatternDividerIcon } from 'assets/pattern-divider.svg';
 import { ReactComponent as DiceIcon } from 'assets/icon-dice.svg';
+import { AdviceType } from 'App';
 
-type AdviceCardType = {
-  title: string;
-  advice: string;
+type AdviceCardType = AdviceType & {
+  handleClick?: () => void;
 };
 
-export const AdviceCard = ({ title, advice }: AdviceCardType) => {
+export const AdviceCard = ({ title, content, handleClick }: AdviceCardType) => {
+  const containerDividerClass = `flex justify-center mt-8 ${handleClick ? 'mb-9' : ''}`;
+
   return (
     <div className="relative bg-neutral-dark-grayish-blue text-center rounded-lg max-w-[540px] px-6 py-10 md:px-10">
       <h3 className="text-primary-neon-cyan uppercase text-xs font-extrabold tracking-widest">
         {title}
       </h3>
-      <p className="text-primary-light-cyan mt-6 text-quote font-extrabold">&quot;{advice}&quot;</p>
-      <PatternDividerIcon className="mx-auto mt-8 mb-9" />
+      <p className="text-primary-light-cyan mt-6 text-quote font-extrabold">
+        &quot;{content}&quot;
+      </p>
+      <div className={containerDividerClass}>
+        <PatternDividerIcon className="w-[295px] h-4" />
+      </div>
 
-      <button className="absolute inset-x-dice -bottom-8 rounded-full bg-primary-neon-cyan hover:shadow-btn flex justify-center items-center w-16 h-16 transition ease-in-out duration-500 hover:rotate-180">
-        <DiceIcon />
-      </button>
+      {handleClick && (
+        <button
+          className="absolute inset-x-dice -bottom-8 rounded-full bg-primary-neon-cyan hover:shadow-btn flex justify-center items-center w-16 h-16 transition ease-in-out duration-500 hover:rotate-180"
+          onClick={handleClick}
+        >
+          <DiceIcon />
+        </button>
+      )}
     </div>
   );
 };
